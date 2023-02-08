eab9b3854ebe4e45beceb4a0f7c764b407028b09
fogelsharp <120275825+fogelsharp@users.noreply.github.com>
2023-02-08 09:59:16 +0100

    changed localhost to ovh

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "mongoURL" : process.env.MONGO_URL || 'localhost',
+  "mongoURL" : process.env.MONGO_URL || 'vps-174c1d29.vps.ovh.net',
   "mongoUser" : process.env.MONGO_USER || '',
   "mongoPass" : process.env.MONGO_PASS || '',
   "mongoDBName" : process.env.MONGO_DB_NAME || 'todos',
